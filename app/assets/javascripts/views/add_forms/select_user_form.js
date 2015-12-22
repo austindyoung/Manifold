be8bb16b87d5a1ea37f1e75ecd5d78337f8f436c
austindyoung <youngaustin.d@gmail.com>
2015-12-22 15:14:52 -0800

    remove commented code

--- app/assets/javascripts/views/add_forms/select_user_form.js
+++ app/assets/javascripts/views/add_forms/select_user_form.js
@@ -1,7 +1,4 @@
 Manifold.Views.SelectUserForm = Backbone.View.extend({
-  events: {
-    // 'click .user': 'addUser'
-  },
 
   className: "dropdown-menu",
 
@@ -22,57 +19,5 @@ Manifold.Views.SelectUserForm = Backbone.View.extend({
 
     return this;
   }
-
-  // addUser: function (event) {
-  //   var selected_user_id = $(event.target).data().id;
-  //   this.model = new Manifold.Models.TeamMembership({
-  //     member_id: selected_user_id,
-  //     project_id: this.model.id
-  //   });
-  //   // var success = function () {
-  //   //   this.collection.add(this.model);
-  //   //   this.model = new Manifold.Models.Project({
-  //   //     organization_id: this.model.get("organization_id")
-  //   //   });
-  //   //   this.render();
-  //   // }.bind(this);
-  //   //
-  //   // function errors(model, response) {
-  //   //   $('.errors').empty();
-  //   //   response.responseJSON.forEach(function (el) {
-  //   //     var $li = $('<li></li>');
-  //   //     $li.text(el);
-  //   //     $('.errors').append($li);
-  //   //   }.bind(this));
-  //   // }
-  //   this.model.save()
-  //   this.remove();
-  // }
-
-  // submit: function (event) {
-  //   event.preventDefault();
-  //   var attrs = $(event.target).serializeJSON();
-  //
-  //   var success = function () {
-  //     this.collection.add(this.model);
-  //     this.model = new Manifold.Models.Workspace({ user_id: Manifold.CURRENT_USER.id })
-  //     this.render();
-  //     //navigate
-  //   }.bind(this);
-  //
-  //   function errors(model, response) {
-  //     $('.errors').empty();
-  //     response.responseJSON.forEach(function (el) {
-  //       var $li = $('<li></li>');
-  //       $li.text(el);
-  //       $('.errors').append($li);
-  //     }.bind(this));
-  //   }
-  //
-  //   this.model.save(attrs, {
-  //     wait: true,
-  //     success: success,
-  //     error: errors.bind(this)
-  //   });
 
 });
