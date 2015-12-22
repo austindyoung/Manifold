58cb5493708e052219dfd31533dd4019c9248969
austindyoung <youngaustin.d@gmail.com>
2015-12-22 11:11:56 -0800

    Update organization_index.js

--- app/assets/javascripts/views/organizations/organization_index.js
+++ app/assets/javascripts/views/organizations/organization_index.js
@@ -15,10 +15,6 @@ Manifold.Views.OrganizationsIndex = Backbone.View.extend({
     this.listenTo(this.collection, 'sync', this.render);
   },
 
-  // creatOrganization: function () {
-  //   var org = new Manifold.Models.Organization({})
-  // },
-
   createOrganization: function () {
     console.log("create");
   },
@@ -27,7 +23,6 @@ Manifold.Views.OrganizationsIndex = Backbone.View.extend({
     var content = this.template({
       organizations: this.collection
     });
-    // todo: use the compositeview pattern
 
     this.$el.html(content);
     return this;
